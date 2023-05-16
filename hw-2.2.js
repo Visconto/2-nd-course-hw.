d013fd073cab68d5817ffa10c8f0c1a5ef2203bd
Visconto <Noukamp@mail.ru>
2023-05-17 01:51:48 +0300

    Прошу проверить мое дз hw2

--- hw-2.2.js
+++ hw-2.2.js
@@ -1,3 +1,74 @@
 // Задание 1
-let a = 10;
-console.log(a);
+// let a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// Задание 2
+// let iphone = 2007;
+// console.log(iphone);
+
+//Задание 3
+// let Name = "Brendan Eich";
+// console.log(Name);
+
+// Задание 4
+
+// let b = 10;
+// let c = 20;
+// console.log(b + c);
+// console.log(b - c);
+// console.log(b * c);
+// console.log(b / c);
+
+//Задание 5
+// let result = 2 ** 5;
+// console.log(result);
+
+// Задание 6
+// let a = 9;
+// let b = 2;
+// console.log(a % b);
+
+// Задание 7
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// console.log(num);
+
+// Задание 8
+// let age = prompt("Сколько вам  лет?");
+// console.log(age);
+
+// Задание 9
+// const user = {
+//   name: "Elmir",
+//   age: 38,
+//   isAdmin: true,
+// };
+// console.log(user);
+
+// Задание 9.1
+// user.cityOfResidence = "Kazan";
+// console.log(user);
+
+// Задание 9.2
+// user[`age`] = 30;
+// console.log(user);
+
+// Задание 9.3
+// delete user.cityOfResidence;
+
+// Задание 9.4
+// let info;
+// info = prompt("Какую информацию хотите узнать о пользователе?");
+// console.log(info);
+
+// Звадание 10
+// let userName = prompt("Как тебя зовут?");
+// console.log("Привет, ${userName}!");
