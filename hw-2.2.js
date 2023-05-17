21f06d14b0f65cc46f5d77045e7702dfda2b3159
Visconto <Noukamp@mail.ru>
2023-05-17 23:55:00 +0300

    самое последнее)))

--- hw-2.2.js
+++ hw-2.2.js
@@ -32,16 +32,19 @@
 
 // Задание 7
 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num += 1;
-// num -= 1;
-// num++;
-// num--;
-// console.log(num);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+num++;
+num--;
+++num;
+--num;
+
+console.log(num);
 
 // Задание 8
 // let age =  prompt("Сколько вам  лет?");
