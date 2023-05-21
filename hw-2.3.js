e08bb2d13c0f26b2bd0ff6ef02cca9bff1fee762
Visconto <Noukamp@mail.ru>
2023-05-22 02:12:50 +0300

    Дом.задание 2.3

---
+++ hw-2.3.js
@@ -0,0 +1,80 @@
+// Задание 1
+
+// let password = "пароль";
+// let enterPassword = prompt("Введите пароль");
+// enterPassword = enterPassword.toLocaleLowerCase();
+// if (password === enterPassword) {
+//   alert("Пароль введен верно");
+// } else {
+//   alert("Пароль введен неправильно");
+// }
+
+// Задание 2
+
+// let a = prompt("Введите число");
+// if (a > 0 && a < 10) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// Задание 3
+
+// let d = prompt("Введите число");
+// let e = prompt("Введите число");
+// if (d > 100 || e > 100) {
+//   console.log("Верно");
+// } else if (d < 100 || e < 100) {
+//   console.log("Неверно");
+// }
+
+// Задание 4
+
+// let a = "2";
+// let b = "3";
+// alert(Number(a) + Number(b));
+
+// Задание 5
+
+// let monthNumber = Number(prompt("введите номер месяца"));
+// switch (monthNumber) {
+//   case 1:
+//     console.log("это первый месяц зимы");
+//     break;
+//   case 2:
+//     console.log("это второй месяц зимы");
+//     break;
+//   case 3:
+//     console.log("это первый месяц весны");
+//     break;
+//   case 4:
+//     console.log("это второй месяц весны");
+//     break;
+//   case 5:
+//     console.log("это третий месяц весны");
+//     break;
+//   case 6:
+//     console.log("это первый месяц лета");
+//     break;
+//   case 7:
+//     console.log("это второй месяц лета");
+//     break;
+//   case 8:
+//     console.log("это третий месяц лета");
+//     break;
+//   case 9:
+//     console.log("это первый месяц осени");
+//     break;
+//   case 10:
+//     console.log("это второй месяц осени");
+//     break;
+//   case 11:
+//     console.log("это третий месяц осени");
+//     break;
+//   case 12:
+//     console.log("это первый месяц зимы");
+//     break;
+//   default:
+//     console.log("такого месяца нет");
+//     break;
+//}
